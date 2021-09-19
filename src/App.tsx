590c1cf1a395da4ef02b38739b3fae2871bc73d3
Kristy Miller <kristy@goodmoney.com>
2021-09-19 14:02:52 -0700

    chore: restructure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Landing from './screens/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Stoic Quotes</h1>
       </header>
+
+      <Landing />
     </div>
   );
 }
---
+++ src/screens/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+  return (
+    <div className="landing">
+      <h2>Call to Action</h2>
+    </div>
+  );
+}
+
+export default Landing;
