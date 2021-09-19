e199548a68c63a1307f3c252b80c2d8398db9212
Kristy Miller <kristy@goodmoney.com>
2021-09-19 14:16:42 -0700

    feat: set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 import Landing from './screens/Landing';
 
-function App() {
-  return (
+export const App = () => (
+  <Router>
     <div className="app">
       <header className="app-header">
         <h1>Stoic Quotes</h1>
       </header>
 
-      <Landing />
+      <Switch>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </Switch>
     </div>
-  );
-}
-
-export default App;
+  </Router>
+);
